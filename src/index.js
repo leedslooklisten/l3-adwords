0425c5b0d26b939b69f1422ac1e81c1c6f62515c
J. Benjamin Leeds <leeds@uw.edu>
2018-07-10 10:48:35 -0700

    add react-router

--- src/index.js
+++ src/index.js
@@ -2,7 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+// import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+ReactDOM.render(
+    <App />, document.getElementById('root') // render the app at the root of the DOM
+);
+
+// disable service worker to start
+// registerServiceWorker();
